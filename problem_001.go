d33a6c94b45cafde0934f8fe86ca3958601fe943
Steve Ardis <steve.ardis@banyanhills.com>
2020-11-06 07:45:06 -0500

    Simple formatting changes

--- problem_001.go
+++ problem_001.go
@@ -9,6 +9,8 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
+// -------------------------------------------------------------------------------------------------
+
 func P0001(cap int, multiplesOf []int) int {
 	sum := 0
 	for number := 1; number < cap; number++ {
